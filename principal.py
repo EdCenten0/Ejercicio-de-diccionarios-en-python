47a79bd89554185212c4c0039f1e4dc923aad107
Ed Centeno <cchavarriacenteno8@gmail.com>
2022-11-05 14:43:38 -0600

    Se importo la libreria os y se agregaron try and except a agregar

--- principal.py
+++ principal.py
@@ -1,20 +1,32 @@
-
+import os
 productos_nombres_precios = {}
 
 
 
 
 def agregar_productos(param_productos_nombres_precios):
+    os.system("cls")
+
     i = 0
     while(i == 0):
         nombre_producto = input("Ingrese el nombre del producto a agregar: ")
-        precio_producto = int(input("Ingrese el precio del producto: "))
+        try:
+            precio_producto = int(input("Ingrese el precio del producto: "))
+        except:
+            print("Por favor, agregue un numero")
+            break
+
         print("Desea agregar otro producto?")
         productos_nombres_precios[nombre_producto] = precio_producto;
 
         print("1. Si")
         print("2. Salir")
-        i = int(input()) - 1
+
+        try:
+            i = int(input()) - 1
+        except:
+            print("Ingrese una opcion valida")
+            i = 0
 
 
 
