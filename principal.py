274a5aea500a46b325bbb0ad9479615e5ea3cd28
Ed Centeno <cchavarriacenteno8@gmail.com>
2022-11-05 14:37:26 -0600

    Primera version de la funcion agregar terminada

--- principal.py
+++ principal.py
@@ -1,14 +1,24 @@
 
+productos_nombres_precios = {}
 
 
 
 
+def agregar_productos(param_productos_nombres_precios):
+    i = 0
+    while(i == 0):
+        nombre_producto = input("Ingrese el nombre del producto a agregar: ")
+        precio_producto = int(input("Ingrese el precio del producto: "))
+        print("Desea agregar otro producto?")
+        productos_nombres_precios[nombre_producto] = precio_producto;
 
-def agregar_productos():
-    n = input("Ingrese el nombre del producto a agregar")
+        print("1. Si")
+        print("2. Salir")
+        i = int(input()) - 1
+
 
 
 def main():
-    print
+    agregar_productos(productos_nombres_precios)
 
 main()
