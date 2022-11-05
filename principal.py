c3a503fb7abc232fb76861f9ccc0580b68928fe0
Ed Centeno <cchavarriacenteno8@gmail.com>
2022-11-05 15:24:35 -0600

    Primera version de leer

--- principal.py
+++ principal.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import os
 productos_nombres_precios = {}
 
@@ -28,7 +29,28 @@ def agregar_productos(param_productos_nombres_precios):
             i = int(input()) - 1
         except:
             print("Ingrese una opcion valida")
-            i = 0
+            menu(param_productos_nombres_precios)
+
+def leer_productos(param_productos_nombres_precios):
+
+    opcion = int(input("Desea leer todos los productos o uno en especifico?1\n1. Todos\n2. Uno es especifico\n"))
+    
+    if(opcion == 1):
+        print(param_productos_nombres_precios)
+    elif(opcion == 2):
+        key = input("Ingrese el nombre del producto del cual desea ver el precio: ")
+        if(param_productos_nombres_precios is not NULL):
+            try:
+                print(key, ': ', param_productos_nombres_precios[key])
+            except:
+                print("El elemento no existe")
+                os.system("pause")
+                menu(param_productos_nombres_precios)
+            
+    else:
+        print("Seleccione una opcion valida")
+        os.system("pause")
+    
 
 
 def menu(param_productos_nombres_precios):
@@ -38,11 +60,14 @@ def menu(param_productos_nombres_precios):
     while(opcion > 0 and opcion < 4):
         print("Que desea hacer?: ")
         print("1. Agregar datos al diccionario")
+        print("2. Leer datos")
         print("4. Salir")
         opcion = int(input())
 
         if(opcion == 1):
             agregar_productos(param_productos_nombres_precios)
+        elif(opcion == 2):
+            leer_productos(param_productos_nombres_precios)
         else:  
             ("Ingrese una opcion valida")
             os.system("pause")
